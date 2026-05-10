c8c8b3788e516e5e09eb78d963e673e07000385f
k3rofr <therealfwilson71@gmail.com>
2026-05-10 16:33:14 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -11,3 +11,20 @@ await octokit.request('GET /repos/{owner}/{repo}/pages', {
     'X-GitHub-Api-Version': '2026-03-10'
   }
 })
+function changeVideo(newSource) {
+    const video = document.getElementById('videoPlayer');
+    
+    // 1. Pause and reset current stream
+    video.pause();
+    
+    // 2. Change source
+    video.src = newSource;
+    
+    // 3. Force the browser to clear the old video and load the new one
+    video.load(); 
+    
+    // 4. Play the new video
+    video.play().catch(error => {
+        console.log("Auto-play was prevented. User must interact first.");
+    });
+}
